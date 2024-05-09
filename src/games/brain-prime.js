a3dd560fdc279e4a477d36f8a68c3bfcbdbbea7c
viktor po <57172087+vic10898@users.noreply.github.com>
2024-05-09 15:06:32 +0300

    Update brain-prime.js
    fixed a bug

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,7 +5,7 @@ const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const game = () => {
   const random = Math.round(Math.random() * 100);
   let result = 'yes';
-  for (let j = 2; j < random / 2; j += 1) {
+  for (let j = 2; j <= random / 2; j += 1) {
     if (random % j === 0) {
       result = 'no';
     }
