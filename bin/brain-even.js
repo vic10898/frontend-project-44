ec0158178c8d937f6720be7881f6df35f866bb1f
vic10898 <57172087+vic10898@users.noreply.github.com>
2024-05-09 11:22:12 +0300

    Update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,33 +1,13 @@
-import readlineSync from 'readline-sync';
-import { name } from './brain-games.js'
+#!/usr/bin/env node
+import play from '../src/index.js';
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-let sum = 0;
-for (let i = 0; i < 3; i += 1) {
-  let random = Math.round(Math.random() * 100)
-  console.log(`Question: ${random}`)
-  const answer = readlineSync.question('');
-  console.log(`Your answer: ${answer}`)
-  if (random % 2 === 0) {
-    if (answer === 'yes') {
-        console.log('Correct!');
-        sum += 1;
-    } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`)
-        console.log(`Let's try again, ${name}!`)
-        break
-    }
-  } else {
-    if (answer === 'no') {
-        console.log('Correct!');
-        sum += 1;
-    } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`)
-        console.log(`Let's try again, ${name}!`)
-        break
-    }
-  }
-}
-if (sum === 3) {
-    console.log(`Congratulations, ${name}!`)
-}
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const game = () => {
+  const random = Math.round(Math.random() * 100);
+  const question = `Question: ${random}`;
+  const result = (random % 2 === 0) ? 'yes' : 'no';
+  return [question, result];
+};
+
+play(rule, game);
