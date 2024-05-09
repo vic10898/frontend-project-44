053642c0d37fddee49c7d6b1a6e4b135811ea905
vic10898 <57172087+vic10898@users.noreply.github.com>
2024-05-09 11:46:21 +0300

    Update brain-prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import play from '../src/index.js';
+
+const rule = 'Find the greatest common divisor of given numbers.';
+
+const game = () => {
+  const random1 = Math.round(Math.random() * 100);
+  const random2 = Math.round(Math.random() * 100);
+  let result = 0;
+  let maxDivisor = Math.min(random1, random2);
+  while (result === 0) {
+    if (random1 % maxDivisor === 0 && random2 % maxDivisor === 0) {
+      result = maxDivisor;
+    } else {
+      maxDivisor -= 1;
+    }
+  }
+  const question = `Question: ${random1} ${random2}`;
+  return [question, result];
+};
+
+play(rule, game);
