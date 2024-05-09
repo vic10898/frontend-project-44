4e2ee2900c7dc271e1221c6cf4de2ee83337f16b
viktor po <57172087+vic10898@users.noreply.github.com>
2024-05-09 13:02:09 +0300

    Update brain-prime.js

---
+++ src/games/brain-prime.js
@@ -0,0 +1,19 @@
+import play from '../index.js';
+
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const game = () => {
+  const random = Math.round(Math.random() * 100);
+  let result = 'yes';
+  for (let j = 2; j < random / 2; j += 1) {
+    if (random % j === 0) {
+      result = 'no';
+    }
+  }
+  const question = `Question: ${random}`;
+  return [question, result];
+};
+
+const gamePrime = () => play(rule, game);
+
+export default gamePrime;
