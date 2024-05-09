3990563641b55182a3bcbc7f8851ebbeeb80d619
viktor po <57172087+vic10898@users.noreply.github.com>
2024-05-09 13:01:15 +0300

    Update brain-calc.js

---
+++ src/games/brain-calc.js
@@ -0,0 +1,23 @@
+import play from '../index.js';
+
+const rule = 'What is the result of the expression?';
+
+const game = () => {
+  const random1 = Math.round(Math.random() * 100);
+  const random2 = Math.round(Math.random() * 100);
+  const operators = {
+    '*': (n1, n2) => n1 * n2,
+    '+': (n1, n2) => n1 + n2,
+    '-': (n1, n2) => n1 - n2,
+  };
+  const ops = ['+', '-', '*'];
+  const opindex = Math.floor(Math.random() * 3);
+  const operator = ops[opindex];
+  const result = operators[operator](random1, random2);
+  const question = `Question: ${random1} ${operator} ${random2}`;
+  return [question, result];
+};
+
+const gameCalc = () => play(rule, game);
+
+export default gameCalc;
